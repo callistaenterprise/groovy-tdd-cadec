6ef8a4a97cc6186510bc5a4e6f5677f124a9289f
Magnus Ekstrand <magnus.ekstrand@callistaenterprise.se>
2013-12-05 09:22:00 +0100

    Utökning av HotelsController med @ResponseBody, @ResponseStatus och @PathVariable

--- src/main/java/org/springframework/webflow/samples/booking/HotelsController.java
+++ src/main/java/org/springframework/webflow/samples/booking/HotelsController.java
@@ -4,47 +4,56 @@ import java.security.Principal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class HotelsController {
 
-    private BookingService bookingService;
+	private BookingService bookingService;
 
-    @Autowired
-    public HotelsController(BookingService bookingService) {
-	this.bookingService = bookingService;
-    }
-
-    @RequestMapping(value = "/hotels/search", method = RequestMethod.GET)
-    public void search(SearchCriteria searchCriteria, Principal currentUser, Model model) {
-	if (currentUser != null) {
-	    List<Booking> booking = bookingService.findBookings(currentUser.getName());
-	    model.addAttribute(booking);
+	@Autowired
+	public HotelsController(BookingService bookingService) {
+		this.bookingService = bookingService;
 	}
-    }
 
-    @RequestMapping(value = "/hotels", method = RequestMethod.GET)
-    public String list(SearchCriteria criteria, Model model) {
-	List<Hotel> hotels = bookingService.findHotels(criteria);
-	model.addAttribute(hotels);
-	return "hotels/list";
-    }
+	@RequestMapping(value = "/hotels/search", method = RequestMethod.GET)
+	@ResponseBody
+	public void search(SearchCriteria searchCriteria, Principal currentUser,
+			Model model) {
+		if (currentUser != null) {
+			List<Booking> booking = bookingService.findBookings(currentUser
+					.getName());
+			model.addAttribute(booking);
+		}
+	}
 
-    @RequestMapping(value = "/hotels/{id}", method = RequestMethod.GET)
-    public String show(@PathVariable Long id, Model model) {
-	model.addAttribute(bookingService.findHotelById(id));
-	return "hotels/show";
-    }
+	@RequestMapping(value = "/hotels", method = RequestMethod.GET)
+	@ResponseBody
+	public String list(SearchCriteria criteria, Model model) {
+		List<Hotel> hotels = bookingService.findHotels(criteria);
+		model.addAttribute(hotels);
+		return "hotels/list";
+	}
 
-    @RequestMapping(value = "/bookings/{id}", method = RequestMethod.DELETE)
-    public String deleteBooking(@PathVariable Long id) {
-	bookingService.cancelBooking(id);
-	return "redirect:../hotels/search";
-    }
+	@RequestMapping(value = "/hotels/{id}", method = RequestMethod.GET)
+	@ResponseBody
+	public String show(@PathVariable("id") Long id, Model model) {
+		model.addAttribute(bookingService.findHotelById(id));
+		return "hotels/show";
+	}
+
+	@RequestMapping(value = "/bookings/{id}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.OK)
+	public String deleteBooking(@PathVariable("id")  Long id) {
+		bookingService.cancelBooking(id);
+		return "redirect:../hotels/search";
+	}
 
 }
